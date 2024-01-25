12144fbba53e7b5151cfe0bdd747575280adcec9
DmitryScaletta <dmitryscaletta@mail.ru>
2024-01-25 18:26:00 +0300

    🩹 Handle child process errors

--- twitch-dlp.js
+++ twitch-dlp.js
@@ -55,12 +55,13 @@ Requires:
 `;
 
 const spawn = (command, args, silent = false) =>
-  new Promise((resolve) => {
+  new Promise((resolve, reject) => {
     const child = childProcess.spawn(command, args);
     if (!silent) {
       child.stdout.on('data', (data) => process.stdout.write(data));
       child.stderr.on('data', (data) => process.stderr.write(data));
     }
+    child.on('error', (err) => reject(err));
     child.on('close', (code) => resolve(code));
   });
 
