4294e3d98c15577cfe28a04fb9430c98d101f7a9
DmitryScaletta <dmitryscaletta@mail.ru>
2025-03-31 13:46:18 +0300

    🩹 Workaround for some old muted highlights

--- src/utils/parseDownloadFormats.ts
+++ src/utils/parseDownloadFormats.ts
@@ -2,14 +2,15 @@ import { parsePlaylist } from '../lib/m3u8.ts';
 import type { DownloadFormat } from '../types.ts';
 
 export const parseDownloadFormats = (playlistContent: string) => {
-  const playlist = parsePlaylist(playlistContent);
   const formats: DownloadFormat[] = [];
-  for (const { name, width, height, url } of playlist) {
+  for (const { name, width, height, url } of parsePlaylist(playlistContent)) {
     formats.push({
       format_id: name.replaceAll(' ', '_'),
       width,
       height,
-      url,
+      // workaround for some old muted highlights
+      // https://regex101.com/r/HdZKlP/1
+      url: url.replace(/-muted-\w+(?=\.m3u8$)/, ''),
     });
   }
   return formats;
