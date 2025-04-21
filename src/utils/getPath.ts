907113033c9ab04fdb8da857127eb611d9660eef
DmitryScaletta <dmitryscaletta@mail.ru>
2025-04-22 00:03:25 +0300

    🩹 Use unicode alternatives for illegal path chars

--- src/utils/getPath.ts
+++ src/utils/getPath.ts
@@ -1,13 +1,28 @@
 import path from 'node:path';
 import type { VideoInfo } from '../types.ts';
 
+const ILLEGAL_PATH_CHARS_MAP: Record<string, string> = {
+  '\\': '⧹',
+  '/': '⧸',
+  ':': '：',
+  '*': '＊',
+  '?': '？',
+  '"': '＂',
+  '<': '＜',
+  '>': '＞',
+  '|': '｜',
+};
+
+const sanitizeFilename = (str: string) =>
+  str.replace(/[\\/:*?"<>|]/g, (c) => ILLEGAL_PATH_CHARS_MAP[c]);
+
 export const getPath = {
   output: (template: string, videoInfo: VideoInfo) => {
     let finalTemplate = template;
     for (const [name, value] of Object.entries(videoInfo)) {
       let newValue = value ? String(value) : '';
       if (name.endsWith('_date')) newValue = newValue.slice(0, 10);
-      newValue = newValue.replace(/[/\\?%*:|"'<>]/g, '');
+      newValue = sanitizeFilename(newValue);
       finalTemplate = finalTemplate.replaceAll(`%(${name})s`, newValue);
     }
     return path.resolve(finalTemplate);
